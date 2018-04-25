87db137a0abee8e1275897cf6d2195d6c84b09ce
stephentetley <stephen.tetley@gmail.com>
2018-04-25 20:35:36 +0100

    Adding more files from fsharp-snippets.

--- src/Test01.fsx
+++ src/Test01.fsx
@@ -1,4 +1,11 @@
-﻿#load "SL\Geo\Coord.fs"
+﻿#I @"..\packages\FParsec.1.0.3\lib\net40-client"
+#r "FParsec"
+#r "FParsecCS"
+
+#load "SL\Geo\Tolerance.fs"
+#load "SL\Geo\Coord.fs"
+#load "SL\Geo\WellKnownText.fs"
+#load "SL\Geo\WebMercator.fs"
 open SL.Geo
 
 
